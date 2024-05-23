8fcc7786a4244df87f9ba253f1a01445ba25bdd9
iannjau <lantanacreations@gmail.com>
2024-05-23 09:56:05 +0300

    add

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,16 @@
-#!/usr/bin/env python3
-
 def return_evens(num_list):
     pass
+    even_numbers = []  
+    for num in num_list:  
+        if num % 2 == 0: 
+            even_numbers.append(num)  
+    return even_numbers 
+
+
+   
+
+        
 
 def make_exclamation(sentence_list):
+    return [sentence + '!' for sentence in sentence_list]
     pass
